517e02f843b34da0755765e78a850c16f6120616
JiaWei <1397311537@qq.com>
2026-03-11 10:28:05 +0800

    项目初始化

---
+++ backend/src/test/java/com/weilair/openagent/OpenagentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weilair.openagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenagentApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
